b7125934070a95715848b54d7556044c6bd1e81b
kvrkarthik <147685505+kvrkarthik@users.noreply.github.com>
2025-06-02 22:04:09 +0530

    Update finale.py

--- finale.py
+++ finale.py
@@ -6,8 +6,7 @@ import os
 import pytesseract
 from skimage.metrics import structural_similarity as ssim
 actual_image_path = "confusionMatrix.jpg"
-pytesseract.pytesseract.tesseract_cmd = r"C:\Users\KARTHIK\AppData\Local\Programs\Tesseract-OCR"
-
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\KARTHIK\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 def preprocess_image_ocr(image_path):
     """Applies preprocessing to improve OCR."""
     try:
