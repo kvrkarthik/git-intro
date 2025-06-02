e16c314c995cf70b23618c3f877dd5d8e62d1321
kvrkarthik <147685505+kvrkarthik@users.noreply.github.com>
2025-06-02 10:17:09 +0530

    Update finale.py

--- finale.py
+++ finale.py
@@ -9,8 +9,7 @@ from skimage.metrics import structural_similarity as ssim
 # Use relative path to avoid platform issues
 actual_image_path = "confusionMatrix.jpg"
 
-# Uncomment and update if running locally
-# pytesseract.pytesseract.tesseract_cmd = r"D:\GenAI\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"D:\GenAI\tesseract.exe"
 
 def preprocess_image_ocr(image_path):
     try:
